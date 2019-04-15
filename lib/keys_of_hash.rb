44a232279255b42d763b99fb0d65879e760d6bb0
Pamyk Charyyeva <pamyk.ch@gmail.com>
2019-04-15 01:05:47 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,11 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    matching_keys = []
+    arguments.each do |arg|
+      self.each do |key, value|
+        matching_keys << key if arg == value
+      end
+    end
+    matching_keys
   end
 end
